49b335f3af253543b2ae1d20c0c4cf2dabe3d88f
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2024-01-03 23:24:48 +0100

    3.01 sklep produkty bestsellery

--- src/Komponenty/Produkt.jsx
+++ src/Komponenty/Produkt.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Produkt = ({ id, name, price }) => (
-    <div key={id} className="product-item">
-        <Link to={`/product/${id}`}>
-            <h3>{name}</h3>
-        </Link>
-        <p>Cena: ${price}</p>
-    </div>
-);
+const Produkt = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3001/products');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+                setProducts(data);
+            } catch (error) {
+                console.error('Error fetching products:', error.message);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h2 className="Naglowki">Lista Bestsellerów</h2>
+            <div className="product-list">
+                {products
+
+                    .map((product) => (
+                        <div key={product.id} className="product-item">
+                            <Link to={`/product/${product.id}`}>
+                                <h3>{product.name}</h3>
+                            </Link>
+                            <p>Cena: ${product.price}</p>
+                        </div>
+                    ))}
+            </div>
+        </div>
+    );
+};
 
 export default Produkt;
--- src/Strony/StronaGlowna.jsx
+++ src/Strony/StronaGlowna.jsx
@@ -1,47 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+
 import './StronyCSS/StronaGlownaCss.css'
+import Produkt from "../Komponenty/Produkt.jsx";
 
 const StronaGlowna = () => {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:3001/products');
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-
-                const data = await response.json();
-                setProducts(data);
-            } catch (error) {
-                console.error('Error fetching products:', error.message);
-            }
-        };
-
-        fetchData();
-    }, []);
 
     return (
         <div>
-            <h2>Lista Produktów</h2>
-            <div className="product-list">
-                {products
-                    .filter((product) => product.bestseller === true)
-                    .map((product) => (
-                        <div key={product.id} className="product-item">
-                            <Link to={`/product/${product.id}`}>
-                                <h3>{product.name}</h3>
-                            </Link>
-                            <p>Cena: ${product.price}</p>
-                        </div>
-                    ))}
-            </div>
+            <Produkt products={bestsellers} />
         </div>
     );
-
-
 };
 
 export default StronaGlowna;
